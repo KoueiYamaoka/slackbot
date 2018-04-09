73399e0ca29f7d5eb548bdff216f0bcb5bed7114
KoueiYamaoka <kouei525@gmail.com>
2018-04-09 20:57:21 +0900

    このハゲー

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,18 +1,26 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 from slackbot.bot import listen_to, respond_to
+
 
 # 例のやつ
 @listen_to('5000兆円')
 @respond_to('5000兆円')
 def many_money(message):
-    message.send(':super5::super0::super0::super0::super_cho::super_yen:\n' \
+    message.send(':super5::super0::super0::super0::super_cho::super_yen:\n'
                  ':space::space::hoshii_left::hoshii_center::hoshii_right:')
 
-# アルファベット，数字，仮名，漢字の後に「欲しい」が続くと何か言ってくる
-@listen_to('([a-zA-Z0-9]|[\u3041-\u3096]|[\u30A1-\u30FA]|' \
-           '[\u3400-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F]|[\uDC00-\uDFFF])欲しい')
-@respond_to('([a-zA-Z0-9]|[\u3041-\u3096]|[\u30A1-\u30FA]|' \
-            '[\u3400-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F]|[\uDC00-\uDFFF])欲しい')
-def super_please(message, args):
+
+# 欲しいっていうとなにか言ってくる
+@listen_to('欲しい')
+@respond_to('欲しい')
+def super_please(message):
     message.send(':hoshii_left::hoshii_center::hoshii_right:')
+
+
+# このハゲーっていうと，違うだろーって言ってくる
+@listen_to('このハゲー')
+@respond_to('このハゲー')
+def konohagee(message):
+    message.send('違うだろー！:face_with_rolling_eyes::triumph::boom::anger:違うだろ！'
+                 ':dizzy_face::anger::rage::anger::bomb:')
